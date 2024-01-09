800cbeebe00cb9184ed3f90af844cfe6ab9a9103
Logan <pix.59700@gmail.com>
2024-01-09 22:42:11 +0100

    Pour un tableau trie

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TP-Eartsup-Gcc2
+---
+## Plus Petit Ecart
+### Pour un tableau trié
+```
+Name: SmallestGapSorted
+input t: tableau trié
+Result int: Retourne la valeur la plus petite entre deux nombre du tableau
+BEGIN
+    let = t[1] - t[0]
+    Pour i=1, à i<taille de t, ++i
+        Si i+1 - i < g alors
+            g = i+1 - i
+        Fin Si
+    Fin Pour
+    Retourne let
+END
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
-int main(){
+int SmallestGapSorted(int* tab, int size){
+    int aux = tab[1] - tab[0];
+    for (int i = 1; i < size-1; ++i) {
+        int gap = tab[i+1]-tab[i];
 
-    std::cout << "ça fonctionne !" << std::endl;
+        if (gap < aux){
+            aux = gap;
+        }
+    }
+    return aux;
+}
+
+int main(){
+    int tab[] = {1, 5, 7, 9, 11, 18};
+    std::cout << SmallestGapSorted(tab, 5) << std::endl;
     return 0;
 }
